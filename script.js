300c3338584255385eb16801d9b7d0c997b44a8e
dharaneesh-sm <sdharaneesh007@gmail.com>
2025-02-05 23:42:04 -0800

    changed image

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('.block').forEach(item => {
+    item.addEventListener('click', function() {
+        document.getElementById('person').firstChild.textContent = this.dataset.name;
+        document.getElementById('photo').src = this.dataset.img;
+    });
+});
+
